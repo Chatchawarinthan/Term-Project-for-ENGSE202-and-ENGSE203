287b17bcfde872e6f5e0c49a646a3d597e19f103
Chatchawarinthan <chatchawarinthan66@live.rmutl.ac.th>
2024-03-11 12:38:54 +0700

    4th

---
+++ Step1/client/dbconfig.js
@@ -0,0 +1,19 @@
+var dbconfig = {
+    development: {
+        //connectionLimit : 10,
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '',
+        database: 'moviedb'
+    },
+    production: {
+        //connectionLimit : 10,
+        host: 'localhost',
+        port: '3306',
+        user: 'dbuser',
+        password: 'P@ssw0rd',
+        database: 'user66049_db'
+    }
+};
+module.exports = dbconfig;
